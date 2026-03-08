242e2fa08c926bd001f277aa56f042a3e940a961
Sugata Kundu <sugatakundu.pro@gmail.com>
2026-03-09 03:17:09 +0530

    chore : active navbar icons

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,6 +1,10 @@
+import JobListings from "../components/JobListings";
+
 const JobsPage = () => {
     return (
-        <div>Jobs Page</div>
+        <section className="bg-blue-50 px-4 py-6">
+            <JobListings />
+        </section>
     )
 }
 export default JobsPage;
